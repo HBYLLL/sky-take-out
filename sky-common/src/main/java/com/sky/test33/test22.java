8be8453f25708b0803dedf2107c4b918706ad40d
ABH <2211970542@qq.com>
2023-11-24 20:03:20 +0800

    push commit

--- sky-common/src/main/java/com/sky/test33/test22.java
+++ sky-common/src/main/java/com/sky/test33/test22.java
@@ -4,5 +4,6 @@ public class test22 {
     public void m1(){
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("oush test");
     }
 }
